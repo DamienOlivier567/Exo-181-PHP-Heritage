a83df98430ff3b96cf29eefc176946b2b260b956
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-02-09 14:19:02 +0100

    Exo-181-PHP

--- classes/Princesse.php
+++ classes/Princesse.php
@@ -5,7 +5,10 @@ class Princesse extends Personnage
 {
     private $saved;
 
-    public function __construct(){
+    public function __construct() {
+
+        parent::__construct();
+
         $this->saved = 0;
         $this->x = 450;
         $this->y = 450;
